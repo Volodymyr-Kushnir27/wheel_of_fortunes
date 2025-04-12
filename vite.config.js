300c6ff8908ce12946329668f306e8468b9363b6
Volodymyr-Kushnir27 <volodumur.kushnir@gmail.com>
2025-04-12 13:40:43 +0300

    New adressn

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/wheel_of_fortune/',
+  base: '/wheel_of_fortunes/',
   plugins: [react()],
 });
 
